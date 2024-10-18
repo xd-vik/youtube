98843e9545913855d9adf5478c41a03d63469b17
strivervishal <strivervishal@gmail.com>
2024-10-18 15:57:27 +0530

    side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import "./App.css";
+import React from "react";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
 import Videos from "./Components/Videos";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="w-full">
+      {/* Header section */}
+      <div className="w-full h-[10%] fixed top-0 z-50">
         <Header />
+      </div>
 
+      {/* Main content (Sidebar and Videos) */}
+      <div className="w-full flex flex-row mt-[4rem] ">
         <Sidebar />
-
         <Videos />
       </div>
     </>
--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -11,7 +11,7 @@ const Videos = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ml-72 p-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ml-72 p-6 w-[83%] absolute left-[-3%] top-[4rem] overflow-y-auto overflow-x-hidden ">
       {videos.map((video) => (
         <div
           key={video.id}
